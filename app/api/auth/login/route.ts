7a7fca6ab42d0faa61d3eaae0d6143a97ddb25e5
dsampson94 <davesampson15@gmail.com>
2025-02-28 10:22:21 +0200

    Initial commit - adds magic link auth, ai integration, comparison tool etc

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,30 +1,29 @@
-// // app/api/auth/login/route.ts
-// import { NextRequest, NextResponse } from 'next/server';
-// import connectToDatabase from '../../../../lib/mongoose';
-// import { verifyPassword, generateToken } from '../../../../lib/auth';
-// import User from '../../../../lib/models/User';
-//
-// export async function POST(req: NextRequest) {
-//     await connectToDatabase();
-//     const { username, password } = await req.json();
-//
-//     if (!username || !password) {
-//         return NextResponse.json({ message: 'Username and password are required.' }, { status: 400 });
-//     }
-//
-//     const user = await User.findOne({ username });
-//
-//     if (!user) {
-//         return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
-//     }
-//
-//     const isValid = await verifyPassword(password, user.password);
-//
-//     if (!isValid) {
-//         return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
-//     }
-//
-//     const token = generateToken({ id: user.id, email: user.email });
-//
-//     return NextResponse.json({ message: 'Login successful', token }, { status: 200 });
-// }
+// app/api/auth/login/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import User from '../../../lib/models/User';
+import connectToDatabase from '../../../lib/mongoose';
+
+export async function POST(req: NextRequest) {
+    // await connectToDatabase();
+    // const { username, password } = await req.json();
+    //
+    // if (!username || !password) {
+    //     return NextResponse.json({ message: 'Username and password are required.' }, { status: 400 });
+    // }
+    //
+    // const user = await User.findOne({ username });
+    //
+    // if (!user) {
+    //     return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    // }
+    //
+    // const isValid = await verifyPassword(password, user.password);
+    //
+    // if (!isValid) {
+    //     return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    // }
+    //
+    // const token = generateToken({ id: user.id, email: user.email });
+    //
+    // return NextResponse.json({ message: 'Login successful', token }, { status: 200 });
+}
