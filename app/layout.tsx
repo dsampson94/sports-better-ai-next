c082e845f07dbb63064ba361b092efad659b94cb
dsampson94 <davesampson15@gmail.com>
2025-03-05 08:22:32 +0200

    adds payments progress etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Sports Better",
-  description: "Sports Better",
+    title: 'Sports Better',
+    description: 'Sports Better',
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body
+            className={ `${ geistSans.variable } ${ geistMono.variable } antialiased` }
+        >
+        { children }
+        </body>
+        <Analytics/>
+        </html>
+    );
 }
