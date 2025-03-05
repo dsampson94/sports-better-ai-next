2b4031b80a885b702d86df42839cd0480f190f35
dsampson94 <davesampson15@gmail.com>
2025-03-05 16:26:20 +0200

    adds overall recommendation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -93,7 +93,7 @@ export default function DashboardPage() {
                 {/* Left: Logo and Title */}
                 <div className="flex items-center space-x-3">
                     <img src="/logos/logo-brain.png" alt="SportsBetter AI Logo" className="h-[120px]" />
-                    <h1 className="block lg:hidden text-xl font-bold">SportsBetter AI 🏆</h1>
+                    <h1 className="lg:block hidden text-xl font-bold">SportsBetter AI 🏆</h1>
                 </div>
                 {/* Right: Query Input, Button & User Dropdown */}
                 <div className="flex items-center space-x-4 relative">
